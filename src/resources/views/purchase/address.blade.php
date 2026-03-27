c2d9bc5e4dbec7d40e4459362d3ce036a121e01c
izumi yuki <uanxpki66q4rrp6cs4kb@outlook.jp>
2026-03-27 23:13:41 +0900

    購入機能作成

--- src/resources/views/purchase/address.blade.php
+++ src/resources/views/purchase/address.blade.php
@@ -2,18 +2,27 @@
 
 @section('title', '住所変更')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+@endsection
+
 @section('content')
 <div class="auth">
     <div class="auth__card auth__card--wide">
         <h1 class="auth__title">住所の変更</h1>
 
-        <form action="{{ route('purchase.address.update', $item) }}" method="POST" class="form">
+        <form action="{{ route('purchase.address.update', $item->id) }}" method="POST" class="form">
             @csrf
             @method('PUT')
 
             <div class="form__group">
                 <label class="form__label">郵便番号</label>
-                <input type="text" name="postal_code" value="{{ old('postal_code', $user->postal_code) }}" class="form__input">
+                <input
+                    type="text"
+                    name="postal_code"
+                    value="{{ old('postal_code') }}"
+                    class="form__input"
+                >
                 @error('postal_code')
                     <p class="form__error">{{ $message }}</p>
                 @enderror
@@ -21,7 +30,12 @@
 
             <div class="form__group">
                 <label class="form__label">住所</label>
-                <input type="text" name="address" value="{{ old('address', $user->address) }}" class="form__input">
+                <input
+                    type="text"
+                    name="address"
+                    value="{{ old('address') }}"
+                    class="form__input"
+                >
                 @error('address')
                     <p class="form__error">{{ $message }}</p>
                 @enderror
@@ -29,13 +43,20 @@
 
             <div class="form__group">
                 <label class="form__label">建物名</label>
-                <input type="text" name="building" value="{{ old('building', $user->building) }}" class="form__input">
+                <input
+                    type="text"
+                    name="building"
+                    value="{{ old('building') }}"
+                    class="form__input"
+                >
                 @error('building')
                     <p class="form__error">{{ $message }}</p>
                 @enderror
             </div>
 
-            <button type="submit" class="button button--primary button--block">更新する</button>
+            <button type="submit" class="button button--primary button--block">
+                更新する
+            </button>
         </form>
     </div>
 </div>
--- src/resources/views/purchase/create.blade.php
+++ src/resources/views/purchase/create.blade.php
@@ -12,7 +12,7 @@
         @csrf
 
         <div class="purchase__main">
-            <div class="purchase-item">
+            <div class="purchase-item panel">
                 <img src="{{ asset($item->image_path) }}" alt="{{ $item->name }} "class="purchase-item__image">
                 <div>
                     <h1 class="purchase-item__name">{{ $item->name }}</h1>
@@ -22,8 +22,8 @@
 
             <div class="panel">
                 <h2 class="panel__title">支払い方法</h2>
-                <select name="payment_method" class="form__select">
-                    <option value="">選択してください</option>
+                <select name="payment_method" id="payment_method" class="form__select" required>
+                    <option value="" disabled selected>選択してください</option>
                     <option value="convenience" {{ old('payment_method') === 'convenience' ? 'selected' : '' }}>コンビニ支払い</option>
                     <option value="card" {{ old('payment_method') === 'card' ? 'selected' : '' }}>カード支払い</option>
                 </select>
@@ -35,10 +35,10 @@
             <div class="panel">
                 <div class="panel__header-row">
                     <h2 class="panel__title">配送先</h2>
-                    <a href="{{ url('/purchase/address/' . $item->id) }}" class="text-link">変更する</a>
+                    <a href="{{ route('purchase.address.edit', $item->id) }}" class="text-link">変更する</a>
                 </div>
-                <p>〒{{ $user->postal_code ?? '' }}</p>
-                <p>{{ $user->address ?? '' }} {{ $user->building ?? '' }}</p>
+                <p>〒{{ $shippingAddress['postal_code'] }}</p>
+                <p>{{ $shippingAddress['address'] }} {{ $shippingAddress['building'] }}</p>
             </div>
         </div>
 
@@ -48,9 +48,33 @@
                     <span>商品代金</span>
                     <span>¥{{ number_format($item->price) }}</span>
                 </div>
-                <button type="submit" class="button button--primary button--block">購入する</button>
+                <div class="summary__row">
+                    <span>支払い方法</span>
+                    <span id="payment_method_view">未選択</span>
+                </div>
+                <button type="submit" class="purchase-button">購入する</button>
             </div>
         </aside>
     </form>
 </div>
+
+<script>
+document.getElementById('payment_method').addEventListener('change', function() {
+
+    const value = this.value;
+
+    let text = "未選択";
+
+    if(value === "convenience"){
+        text = "コンビニ支払い";
+    }
+
+    if(value === "card"){
+        text = "カード支払い";
+    }
+
+    document.getElementById('payment_method_view').textContent = text;
+
+});
+</script>
 @endsection
